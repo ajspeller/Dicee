6274d48ece518ae58728f26772b755acc2a83e47
AJ Speller <ajspeller@gmail.com>
2017-08-01 17:40:46 -0400

    Create button listener

--- app/src/main/java/com/ajspeller/dicee/MainActivity.java
+++ app/src/main/java/com/ajspeller/dicee/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ajspeller.dicee;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = (Button) findViewById(R.id.rollBtn);
+
+        ImageView leftDie = (ImageView) findViewById(R.id.leftDie);
+        ImageView rightDie = (ImageView) findViewById(R.id.rightDie);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Dicee", "onClick: The button has been pressed");
+            }
+        });
     }
 }
